0c4cbc223e90218df8c2a2685b50904aa3a2cf79
takkanm <takkanm@gmail.com>
2016-12-20 20:18:28 +0900

    user set delegate variable name

--- lib/strong_delegate.rb
+++ lib/strong_delegate.rb
@@ -8,7 +8,10 @@ module StrongDelegate
   end
 
   module ClassMethods
-    def def_delegate(delegate_class = nil, &block)
+    attr_read :delegate_variable_name
+
+    def def_delegate(delegate_variable_name = nil, &block)
+      @delegate_variable_name = delegate_variable_name
 
       obj = Object.new
       obj.singleton_class.class_eval &block
@@ -17,8 +20,6 @@ module StrongDelegate
         m = obj.singleton_class.instance_method(method_name)
         delegate_methods[method_name] = m.parameters
       end
-
-      delegate_to delegate_class if delegate_class
     end
 
     def assert_delegate!(delegate_class)
@@ -28,14 +29,8 @@ module StrongDelegate
       end
     end
 
-    def delegate_to(delegate_class)
-      assert_delegate! delegate_class
-
-      @delegate_class = delegate_class
-    end
-
-    def delegate_class
-      @delegate_class
+    def delegate_to(delegate_variable_name)
+      @delegate_variable_name = delegate_variable_name
     end
 
     def delegate_methods
@@ -44,8 +39,6 @@ module StrongDelegate
   end
 
   def method_missing(name, *args, &block)
-    @__delegate_object__ ||= self.class.delegate_class.new
-
     if self.class.delegate_methods.key?(name.to_sym)
       invoke_delegate_method!(name.to_sym, args, block)
     else
@@ -53,23 +46,19 @@ module StrongDelegate
     end
   end
 
+  def delegate_object
+    instance_variable_get self.class.delegate_variable_name
+  end
+
   def invoke_delegate_method!(name, args, block)
-    delegate_method = self.class.delegate_methods[name]
+    object = delegate_object
+    assert_delegate!(object, name.to_sym)
 
-    assert_arity!(delegate_method[:arity], args)
-    assert_block!(delegate_method, block)
-
-    object = instance_variable_get(self.class.delegate_object_name)
     object.public_send(*args, &block)
   end
 
-  def assert_arity!(arity, args)
-    return if arity < 0
-    raise unless block_given == args.size
-  end
-
-  def assert_arity(block_given, block)
-    return if block_given.nil?
-    raise unless block_given == !!(block_given)
+  def assert_delegate!(object, name)
+    return if object.method(name).parameters == self.class.delegate_methods[name]
+    raise StrongDelegate::InvalidInterfaceError
   end
 end
